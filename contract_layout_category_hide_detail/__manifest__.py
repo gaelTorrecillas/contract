8f049ebbfff8daedf853bc2bfd4032a11049207a
Víctor Martínez <victor.martinez@tecnativa.com>
2020-10-16 11:43:13 +0200

    [MIG] contract_layout_category_hide_detail: Migration to 13.0

--- contract_layout_category_hide_detail/__manifest__.py
+++ contract_layout_category_hide_detail/__manifest__.py
@@ -4,11 +4,16 @@
 {
     "name": "Contract layout category hide detail",
     "summary": "Hide details for sections in Contract lines",
+<<<<<<< HEAD
     "version": "14.0.1.0.0",
+=======
+    "version": "13.0.1.0.0",
+>>>>>>> [MIG] contract_layout_category_hide_detail: Migration to 13.0
     "category": "Contract Management",
     "website": "https://github.com/OCA/contract",
     "author": "Tecnativa, " "Odoo Community Association (OCA), " "Mind And Go, ",
     "license": "AGPL-3",
     "depends": ["contract", "sale_layout_category_hide_detail"],
+    "development_status": "Production/Stable",
     "data": ["views/abstract_contract_line.xml", "views/contract.xml"],
 }
